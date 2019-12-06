8934841cb7a5cbd510a1a2f9dedf861f22ec17b5
Roby Tanama <tanamaroby@gmail.com>
2019-12-07 07:49:17 +0800

    Add animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Animator animator;
+
+    public bool attack = false;
+    public bool defend = false;
+    public bool magic = false;
+    public bool heal = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (attack)
+        {
+            animator.SetBool("isAttacking", true);
+        }
     }
 }
