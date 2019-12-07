9cb5952b26d9a7c2a1fbf8c6eadec499c8ceaa8d
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-12-07 13:05:17 +0800

    Integrate game over button

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnReturn);
+    }
+
+    void OnReturn()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
