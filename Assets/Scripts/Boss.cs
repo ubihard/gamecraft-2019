91a22959c0cd10ce57ee1a22fa469c58596d200c
Roby Tanama <tanamaroby@gmail.com>
2019-12-07 13:23:32 +0800

    commit man

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boss : MonoBehaviour
 {
@@ -10,13 +11,27 @@ public class Boss : MonoBehaviour
     public int defense = 20;
     public int resistance = 20;
 
+    public int currentSceneIndex;
+
+    public 
+
     void Start()
     {
         health = originalHealth;
 
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
         EventManager.Instance.playerAttackEvent.AddListener(OnPhysicalHit);
         EventManager.Instance.playerMagicEvent.AddListener(OnMagicalHit);
         EventManager.Instance.bossAttackFilledEvent.AddListener(OnAttack);
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
     }
 
     void OnPhysicalHit(int damage)
